297fe3accd18cce71f24fe510a3e6edcd8303403
Mikael Lindqvist <li.mikael@gmail.com>
2016-10-11 15:17:33 +0200

    strip slashes

--- endpoint.php
+++ endpoint.php
@@ -7,6 +7,12 @@ require_once __DIR__."/ext/minixapi/MiniXapi.php";
 use xapilrs\WpUtil;
 global $wpdb;
 
+// Remote magic quotes, WordPress always adds these. Refernce:
+// http://stackoverflow.com/questions/8949768/with-magic-quotes-disabled-why-does-php-wordpress-continue-to-auto-escape-my
+// https://core.trac.wordpress.org/ticket/18322
+foreach ($_REQUEST as $k=>$v)
+	$_REQUEST[$k]=stripslashes($v);
+
 $miniXapi=new MiniXapi();
 $miniXapi->setPdo(WpUtil::getCompatiblePdo());
 $miniXapi->setTablePrefix($wpdb->prefix);
